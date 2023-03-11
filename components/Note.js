3a6a13993f667f48c7d5aa4922da9ca1533866b3
Naman Varshney <namanvarshney09@gmail.com>
2023-03-11 18:07:30 +0530

    Added Note component

---
+++ components/Note.js
@@ -0,0 +1,25 @@
+import React, { useContext, useRef, useState } from 'react'
+import styles from '@/styles/Note.module.css'
+import noteContext from '@/context/notes/noteContext';
+
+const Note = (props) => {
+    const { note, updateNote } = props;
+    const context = useContext(noteContext);
+    const { deleteNote } = context;
+    return (
+        <>
+            <div className={styles.card}>
+                <div className={styles.card_title}>
+                    <span >{note.title}</span>
+                    <div>
+                        <i className="fa-regular fa-pen-to-square" onClick={() => updateNote(note)}></i>
+                        <i className="fa-solid fa-trash" onClick={() => { deleteNote(note._id) }}></i>
+                    </div>
+                </div>
+                <div className={styles.card_description}>{note.description}</div>
+            </div>
+        </>
+    )
+}
+
+export default Note
