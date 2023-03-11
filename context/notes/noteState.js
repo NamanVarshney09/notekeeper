83eb49d5527a8c5de0443d0293abb9490bc7fb25
Naman Varshney <namanvarshney09@gmail.com>
2023-03-11 18:06:00 +0530

    Added a note context for CRUD operations

---
+++ context/notes/noteState.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import NoteContext from "./noteContext";
+
+const NoteState = (props) => {
+  const host = "http://localhost:5000";
+
+  const [notes, setNotes] = useState([])
+
+  const fetchNotes = async () => {
+    const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQwNzM3MmMzNDdhZjYxNmY1ZWM0YjY3In0sImlhdCI6MTY3ODE5OTA4N30.Dy-IbKHigPNsDsSCvdAhQAcyeExIixYo6zy0ofWR37M",
+      }
+    });
+    const data = await response.json();
+    setNotes(data);
+  }
+
+  const addNote = async (title, description, tag) => {
+    const response = await fetch(`${host}/api/notes/addnote`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQwNzM3MmMzNDdhZjYxNmY1ZWM0YjY3In0sImlhdCI6MTY3ODE5OTA4N30.Dy-IbKHigPNsDsSCvdAhQAcyeExIixYo6zy0ofWR37M",
+      },
+      body: JSON.stringify({ title, description, tag })
+    });
+    const note = await response.json();
+    setNotes([...notes, note]);
+  }
+
+  const editNote = async (id, title, description, tag) => {
+    const response = await fetch(`${host}/api/notes/updatenote/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQwNzM3MmMzNDdhZjYxNmY1ZWM0YjY3In0sImlhdCI6MTY3ODE5OTA4N30.Dy-IbKHigPNsDsSCvdAhQAcyeExIixYo6zy0ofWR37M",
+      },
+      body: JSON.stringify({ title, description, tag })
+    });
+    const data = await response.json();
+
+    const updatedNotes = notes.map((note) => {
+      if (note._id === id) {
+        {
+          note.title = title;
+          note.description = description;
+          note.tag = tag;
+        }
+      }
+      return note;
+    })
+    setNotes(updatedNotes)
+  }
+
+  const deleteNote = async (id) => {
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQwNzM3MmMzNDdhZjYxNmY1ZWM0YjY3In0sImlhdCI6MTY3ODE5OTA4N30.Dy-IbKHigPNsDsSCvdAhQAcyeExIixYo6zy0ofWR37M",
+      }
+    });
+    const data = await response.json();
+    const filteredNotes = notes.filter((note) => { return note._id !== id })
+    setNotes(filteredNotes)
+  }
+  return (
+    <NoteContext.Provider value={{ notes, setNotes, addNote, editNote, deleteNote, fetchNotes }}>
+      {props.children}
+    </NoteContext.Provider>
+  )
+}
+
+export default NoteState
